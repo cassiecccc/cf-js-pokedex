8c6eb002c8185b7cf480f8341d78dd90c83f793a
Cassie <101525140+cassiecccc@users.noreply.github.com>
2023-03-26 19:36:10 -0400

    Updated pokemonList

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-alert("Hello World");
-let favoriteFood = "lychee";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Squirtle", height: "0.5m", types: ["water"] },
+  { name: "Dugtrio", height: "0.7m", types: ["ground"] },
+  { name: "Machop", height: "0.8m", types: ["fighting"] },
+  { name: "Eevee", height: "0.3m", types: ["normal"] },
+  { name: "Slowbro", height: "1.6m", types: ["psychic", "water"] },
+];
