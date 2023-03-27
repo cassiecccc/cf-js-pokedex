3c8e7de82f06781cd8008baae09236b049aad7e7
Cassie <101525140+cassiecccc@users.noreply.github.com>
2023-03-27 17:05:46 -0400

    Updated IIFE

--- js/script.js
+++ js/script.js
@@ -1,27 +1,60 @@
-let pokemonList = [
-  { name: "Squirtle", height: "0.5", types: ["water"] },
-  { name: "Dugtrio", height: "0.7", types: ["ground"] },
-  { name: "Machop", height: "0.8", types: ["fighting"] },
-  { name: "Eevee", height: "0.3", types: ["normal"] },
-  { name: "Slowbro", height: "1.6", types: ["psychic", "water"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Squirtle", height: "0.5", types: ["water"] },
+    { name: "Dugtrio", height: "0.7", types: ["ground"] },
+    { name: "Machop", height: "0.8", types: ["fighting"] },
+    { name: "Eevee", height: "0.3", types: ["normal"] },
+    { name: "Slowbro", height: "1.6", types: ["psychic", "water"] },
+  ];
 
-//changed for loop to forEach
-pokemonList.forEach((pokemon) =>
-  pokemon.height > 1
-    ? document.write(
-        "<p>" +
-          pokemon.name +
-          " " +
-          "(height" +
-          " " +
-          pokemon.height +
-          ")" +
-          " " +
-          "Wow, that's big!" +
-          "</p>"
-      )
-    : document.write(
-        "<p>" + pokemon.name + " " + "(height" + " " + pokemon.height + ")"
-      ) + "</p>"
-);
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object"
+      //  &&
+      // // Object.keys(pokemon) === Object.keys(pokemonList)
+    ) {
+      return pokemonList.push(pokemon);
+    } else {
+      console.warn("Please add an object");
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+//test add function
+let newPokemon = {
+  name: "Psyduck",
+  height: "0.8",
+  type: "water",
+};
+
+pokemonRepository.add(newPokemon);
+
+pokemonRepository
+  .getAll()
+  .forEach((pokemon) =>
+    pokemon.height > 1
+      ? document.write(
+          "<p>" +
+            pokemon.name +
+            " " +
+            "(height" +
+            " " +
+            pokemon.height +
+            ")" +
+            " " +
+            "Wow, that's big!" +
+            "</p>"
+        )
+      : document.write(
+          "<p>" + pokemon.name + " " + "(height" + " " + pokemon.height + ")"
+        ) + "</p>"
+  );
