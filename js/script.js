b14ea265005f7786c67c8deb7e386d79461451c5
Cassie <101525140+cassiecccc@users.noreply.github.com>
2023-03-27 14:18:09 -0400

    print pokemonlist to html and added height condition

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 let pokemonList = [
-  { name: "Squirtle", height: "0.5m", types: ["water"] },
-  { name: "Dugtrio", height: "0.7m", types: ["ground"] },
-  { name: "Machop", height: "0.8m", types: ["fighting"] },
-  { name: "Eevee", height: "0.3m", types: ["normal"] },
-  { name: "Slowbro", height: "1.6m", types: ["psychic", "water"] },
+  { name: "Squirtle", height: "0.5", types: ["water"] },
+  { name: "Dugtrio", height: "0.7", types: ["ground"] },
+  { name: "Machop", height: "0.8", types: ["fighting"] },
+  { name: "Eevee", height: "0.3", types: ["normal"] },
+  { name: "Slowbro", height: "1.6", types: ["psychic", "water"] },
 ];
+
+//iterate over and log pokemonList
+for (let i = 0; i <= pokemonList.length; i++) {
+  //add height conditions
+  if (pokemonList[i].height > 1) {
+    document.write(
+      `${pokemonList[i].name} height(${pokemonList[i].height}) - Wow, that's big!` +
+        "<br>"
+    );
+  } else {
+    document.write(
+      `${pokemonList[i].name} height(${pokemonList[i].height})` + "<br>"
+    );
+  }
+}
