c490baed30e6394c2b964f79b846c51de6898ea5
Cassie <101525140+cassiecccc@users.noreply.github.com>
2023-03-28 13:27:00 -0400

    replace docu.write with DOM

--- js/script.js
+++ js/script.js
@@ -5,56 +5,48 @@ let pokemonRepository = (function () {
     { name: "Machop", height: "0.8", types: ["fighting"] },
     { name: "Eevee", height: "0.3", types: ["normal"] },
     { name: "Slowbro", height: "1.6", types: ["psychic", "water"] },
+    { name: "Psyduck", height: "0.8", type: ["water"] },
   ];
-
-  function add(pokemon) {
-    if (
-      typeof pokemon === "object"
-      //  &&
-      // // Object.keys(pokemon) === Object.keys(pokemonList)
-    ) {
-      return pokemonList.push(pokemon);
-    } else {
-      console.warn("Please add an object");
-    }
-  }
 
   function getAll() {
     return pokemonList;
   }
 
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  //update DOM
+  function addListItem(pokemon) {
+    const pokemonList = document.querySelector(".pokemon-list");
+    let listItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.classList.add("name-button");
+    button.innerText = pokemon.name;
+    listItem.appendChild(button);
+    clickButton(button, pokemon);
+    pokemonList.appendChild(listItem);
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  //click function
+  function clickButton(button, pokemon) {
+    button.addEventListener("click", () => {
+      pokemonRepository.showDetails(pokemon);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
   };
 })();
 
-//test add function
-let newPokemon = {
-  name: "Psyduck",
-  height: "0.8",
-  type: "water",
-};
-
-pokemonRepository.add(newPokemon);
-
-pokemonRepository
-  .getAll()
-  .forEach((pokemon) =>
-    pokemon.height > 1
-      ? document.write(
-          "<p>" +
-            pokemon.name +
-            " " +
-            "(height" +
-            " " +
-            pokemon.height +
-            ")" +
-            " " +
-            "Wow, that's big!" +
-            "</p>"
-        )
-      : document.write(
-          "<p>" + pokemon.name + " " + "(height" + " " + pokemon.height + ")"
-        ) + "</p>"
-  );
+pokemonRepository.getAll().forEach((pokemon) => {
+  pokemonRepository.addListItem(pokemon);
+});
